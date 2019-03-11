68002467af1a361334fb050a3c7fb0d2c4aab781
Natalia Sochina <natalia.sochina@gmail.com>
2019-03-11 16:46:00 +0300

    add MainClass and test

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,7 +1,2 @@
 public class MainClass {
-    private int class_number = 20;
-
-    public int getClassNumber() {
-        return class_number;
-    }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,14 +1,3 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 public class MainClassTest {
-
-    @Test
-    public void testGetClassNumber()
-    {
-        MainClass mainClass = new MainClass();
-        assertTrue("class_number NOT more 45", (mainClass.getClassNumber() >= 45) );
-    }
-
 }
 
